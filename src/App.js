9c766d1468e8b33a1816a8387962c5fce83f6583
mohamed ibarahim <mohamedibrahim.zeka@gmail.com>
2024-04-17 12:56:53 +0200

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [x, setx] = useState([]);
+
+  const inputRef = useRef();
+
+  const add = () => {
+    const value = inputRef.current.value;
+    const newData = { comleted: false, value };
+    setx([...x, newData]);
+
+    inputRef.current.value = "";
+  };
+
+  const itemDone = (index) => {
+    const newx = [...x];
+    newx[index].comleted = !newx[index].comleted;
+    setx(newx);
+  };
+
+  const toDelete = (index) => {
+    const newx = [...x];
+    newx.splice(index, 1);
+    setx(newx);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> to do list</h2>
+
+      <ul>
+        {x.map(({ value, comleted }, index) => {
+          return (
+            <div className="div10">
+              <li
+                className={comleted ? "diffstyle" : ""}
+                onClick={() => itemDone(index)}
+              >
+                {value}
+              </li>
+              <span onClick={() => toDelete(index)}>x</span>
+            </div>
+          );
+        })}
+      </ul>
+
+      <input ref={inputRef} placeholder="Enter new task...." />
+
+      <button onClick={add}>Add</button>
     </div>
   );
 }
